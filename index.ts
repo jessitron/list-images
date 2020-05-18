5602d2d6abc60cbbf35e665a94192be241a34cb1
Jessica Kerr <jessitron@gmail.com>
2020-05-17 20:36:07 -0500

    getting htere

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
 
 console.log("GIVE ME YOUR MARKDOWN");
+
+const files = process.argv.filter(s => s.endsWith(".md"));
+
+console.log("The files are: " + files.join(" "))
+
+if (files.length === 0) {
+  console.log("USAGE: list-images <name of markdown file...>");
+  process.exit(1);
+}
+
